0f03187be6eb324f293627b36c720da06baf43da
Thiago Santos <thiago.moura@ccc.ufcg.edu.br>
2017-06-23 19:36:11 -0300

    Atualização

--- AndroidAsyncTask/NotificacaoApp/gen/br/edu/ifpb/notificacaoapp/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package br.edu.ifpb.notificacaoapp;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- AndroidAsyncTask/NotificacaoApp/src/br/edu/ifpb/util/StringUtil.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifpb.util;
-
-/**
- * Created by Rhavy on 17/12/2015.
- */
-public class StringUtil {
-
-    public static final String VAZIO = "";
-}
